4bc5a35965387fdb884117bc3d993064cf008b99
Leovin <Leovinjozhsalarda@gmail.com>
2026-01-30 09:04:04 +0800

    add: mision and vision section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { StickyNavbar } from "./components/navbar";
 import { HeroSection } from "./pages/home";
 import { AboutSection } from "./pages/about";
-import { VisionMissionSection } from "./pages/misionandvision";
+import { VisionMissionSection } from "./pages/missionandvision";
 
 export default function App() {
   return (
