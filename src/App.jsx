44c23fc638f0c5fe703012dff3c897ee4aecb33f
Ian Glenn Curilan <iancurilan1027.com>
2026-01-30 09:17:36 +0800

    add: info section

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ export default function App() {
         <VisionMissionSection />
         <ResearchAreasSection />
         <ProjectsSection />
+        <InfoSection />
       </main>
     </div>
   );
