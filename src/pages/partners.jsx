47d6d1dd5685276f7e8cc58f8dae639ecfd0f1b8
Leovin <Leovinjozhsalarda@gmail.com>
2026-02-04 11:23:17 +0800

    feat: add link

--- src/pages/partners.jsx
+++ src/pages/partners.jsx
@@ -1,12 +1,12 @@
 "use client"
 
 const partners = [
-  { name: "DOST", img: "/dost.jpg" },
-  { name: "CSU", img: "/CSU.png" },
-  { name: "DOA", img: "/DOA.png" },
-  { name: "TMC", img: "/tmc.png" },
-  { name: "APEX", img: "/apex.png" },
-  { name: "DSWD", img: "/dswd.png" },
+  { name: "DOST", img: "/dost.jpg", href: "https://www.dost.gov.ph/" },
+  { name: "CSU", img: "/CSU.png", href: "https://www.carsu.edu.ph/" },
+  { name: "DOA", img: "/DOA.png", href: "https://www.da.gov.ph/" },
+  { name: "TMC", img: "/tmc.png", href: "https://www.mgb.gov.ph/" },
+  { name: "APEX", img: "/apex.png", href: "https://www.apexmines.com/" },
+  { name: "DSWD", img: "/dswd.png", href: "https://caraga.dswd.gov.ph/" },
 ]
 
 export function PartnersSection() {
@@ -19,25 +19,38 @@ export function PartnersSection() {
       <div className="partners mx-auto max-w-7xl py-4" data-aos="fade-up">
         <div className="partners-wrapper">
           
-          {/* First slide */}
+           {/* First slide */}
           <div className="partners-slide">
             {partners.map((partner) => (
-              <div key={partner.name} className="partner-item">
+              <a
+                key={partner.name}
+                href={partner.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="partner-item"
+              >
                 <img src={partner.img} alt={partner.name} />
                 <p>{partner.name}</p>
-              </div>
+              </a>
             ))}
           </div>
 
-          {/* Second slide (duplicate for infinite loop effect) */}
+          {/* Second slide */}
           <div className="partners-slide">
             {partners.map((partner) => (
-              <div key={partner.name + "-2"} className="partner-item">
+              <a
+                key={partner.name + "-2"}
+                href={partner.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="partner-item"
+              >
                 <img src={partner.img} alt={partner.name} />
                 <p>{partner.name}</p>
-              </div>
+              </a>
             ))}
           </div>
+
 
         </div>
       </div>
